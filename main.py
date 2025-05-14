1ae803057982fe0c9af756604a83cbe6cd15617d
Douglas Wiercinski <dougwier@talvie.com>
2025-05-14 14:30:40 -0700

    CH3: Report L1: Print a Report

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from stats import get_book_wc, get_book_cc
+from stats import get_book_wc, get_book_cc, sort_char_list
 
 def main():
-    file_path = "./books/frankenstein.txt"
-    with open(file_path) as f:
-        file_c = f.read()
-    get_book_wc(file_c)
-    get_book_cc(file_c)
+    book_path = "./books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_book_wc(text)
+    chars_dict = get_book_cc(text)
+    sort_dict = sort_char_list(chars_dict)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for i in sort_dict:
+        print(f"{i["letter"]}: {i["num"]}")
+    print("============= END ===============")
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,13 +1,26 @@
 def get_book_wc(book_text):
     num_lines = len(book_text.split())
-    print(f"{num_lines} words found in the document")
+    return num_lines
 
 def get_book_cc(book_text):
     lower_book_text = book_text.lower()
     char_counts = {}
     for chars in lower_book_text:
         if chars in char_counts:
-            char_counts[chars] = char_counts[chars] + 1
+            char_counts[chars] += 1
         else:
             char_counts[chars] = 1
-    print(char_counts)
+    return char_counts
+
+def sort_on(dict):
+    return dict['num']
+
+def sort_char_list(char_counts):
+    sorting_a = []
+    temp_dict = {}
+    for k in char_counts:
+        temp_dict.clear()
+        if k.isalpha():
+            sorting_a.append({"letter": k, "num": char_counts[k]})
+    sorting_a.sort(reverse=True, key=sort_on)
+    return sorting_a
