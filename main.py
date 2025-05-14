0650c3f521f86bd7a1b4003854c852c1de121a89
Douglas Wiercinski <dougwier@talvie.com>
2025-05-14 14:54:28 -0700

    CH3: Report L2 Arguments

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import sys
 from stats import get_book_wc, get_book_cc, sort_char_list
 
 def main():
-    book_path = "./books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_book_wc(text)
     chars_dict = get_book_cc(text)
     sort_dict = sort_char_list(chars_dict)
+    print_report(book_path, num_words, sort_dict)
+
+def print_report(book_path, num_words, sort_dict):
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
